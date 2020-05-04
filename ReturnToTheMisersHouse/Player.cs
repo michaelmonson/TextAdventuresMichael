b4f83c3b1fbb44e9afaecaabdec4cc5fd0c7068d
Michael Monson <mmonson@ancestry.com>
2020-05-03 22:17:56 -0600

    Converted Player recognition into a switch structure with case statements.  This replaces the 'if else' series.

--- ReturnToTheMisersHouse/Player.cs
+++ ReturnToTheMisersHouse/Player.cs
@@ -10,33 +10,42 @@ namespace ReturnToTheMisersHouse
 
         public string processPlayerName(string userEnteredName, string playerName)
         {
-            if (userEnteredName.Trim().Equals("SARIAH", StringComparison.InvariantCultureIgnoreCase))
+            
+            switch (userEnteredName.Trim().ToUpper())
             {
-                playerName = "Princess " + userEnteredName;
-                Console.Write($"{dl} Sariah!  I know thee!  But thou art a Princess of the Lord! Welcome to the Miser's House, '{playerName}!'");
-            }
-            else if (userEnteredName.Trim().Equals("RUTH", StringComparison.InvariantCultureIgnoreCase))
-            {
-                playerName = "Foxy " + userEnteredName;
-                Console.Write($"{dl} Ruth!  I know thee!  You are a friend of all foxes! Welcome to the Miser's House, '{playerName}!'");
-            }
-            else if (userEnteredName.Trim().Equals("MICHAEL", StringComparison.InvariantCultureIgnoreCase))
-            {
-                playerName = "Sir Orlando";
-                Console.Write($"{dl} Ah my friend!  You are one of our own!  Welcome back to the Miser's House, '{playerName}!'");
-            }
-            else if (userEnteredName.Trim().Length > 0)
-            {
-                playerName = userEnteredName;
-                Console.Write($"{dl} Welcome {playerName}!  Let us begin your adventure this day!'");
-            }
-            else
-            {
-                Console.Write($"{dl} Very well... if thou shall not reveal thy true identity, I shall call thee... '{playerName}!'");
+                case "MICHAEL":
+                    playerName = "Sir Orlando";
+                    Console.Write($"{dl} Ah my friend!  You are one of our own!  Welcome back to the Miser's House, '{playerName}!'");
+                    break;
+                case "SARIAH":
+                    playerName = "Princess " + userEnteredName;
+                    Console.Write($"{dl} Sariah!  I know thee!  But thou art a Princess of the Lord! Welcome to the Miser's House, '{playerName}!'");
+                    break;
+                case "RUTH":
+                    playerName = "Foxy " + userEnteredName;
+                    Console.Write($"{dl} Ruth!  I know thee!  You are a friend of all foxes! Welcome to the Miser's House, '{playerName}!'");
+                    break;
+                case "CELESTE":
+                    playerName = userEnteredName;
+                    Console.Write($"{dl} Celeste!  Welcome to the old Miser's house... did you know your father visited here on a Commodore64 interface?");
+                    break;
+                default:
+                    if (userEnteredName.Trim().Length > 0)
+                    {
+                        playerName = userEnteredName;
+                        Console.Write($"{dl} Welcome {playerName}!  Let us begin your adventure this day!'");
+                    } 
+                    else
+                    {
+                        Console.Write($"{dl} Very well... if thou shall not reveal thy true identity, I shall call thee... '{playerName}!'");
+                    }
+                    break;
+
             }
 
             return playerName;
         }
+        
 
     }
 }
